d1263b9c0b478ef89d0c40e267595d9d33f14fd2
Sergey <fserega2014@yandex.ru>
2023-07-28 23:04:53 +0300

    feat: add users crud methods

---
+++ src/user/user.controller.ts
@@ -0,0 +1,57 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+import { plainToClass } from 'class-transformer';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  findAll(): User[] {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    if (!this.userService.isValidUserId(id)) {
+      throw new BadRequestException('Invalid userId');
+    }
+    const user = this.userService.findOne(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() createUserDto: CreateUserDto): User {
+    return plainToClass(User, this.userService.create(createUserDto));
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return plainToClass(User, this.userService.update(id, updateUserDto));
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id: string) {
+    this.userService.remove(id);
+  }
+}
