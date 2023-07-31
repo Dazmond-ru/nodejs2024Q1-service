acd97fd06298da440c296dce726ea25b9e81c2f6
Sergey <fserega2014@yandex.ru>
2023-07-31 23:28:52 +0300

    feat: add artists doc

--- src/artist/dto/update-artist.dto.ts
+++ src/artist/dto/update-artist.dto.ts
@@ -1,12 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateArtistDto } from './create-artist.dto';
 import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateArtistDto extends PartialType(CreateArtistDto) {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsBoolean()
   @IsNotEmpty()
   grammy: boolean;
