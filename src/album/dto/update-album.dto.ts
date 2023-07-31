b28a756079e6a32286d7fd6fb726883a5c7dd459
Sergey <fserega2014@yandex.ru>
2023-07-31 23:08:52 +0300

    feat: add albums doc

--- src/album/dto/update-album.dto.ts
+++ src/album/dto/update-album.dto.ts
@@ -1,11 +1,16 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAlbumDto } from './create-album.dto';
 import { IsOptional, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateAlbumDto extends PartialType(CreateAlbumDto) {
+  @ApiProperty({ required: false })
   name: string;
+
+  @ApiProperty({ required: false })
   year: number;
 
+  @ApiProperty({ required: false })
   @IsString()
   @IsOptional()
   artistId: string | null;
