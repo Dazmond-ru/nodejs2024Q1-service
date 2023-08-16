b9872893444998f759a0591cd4def5fbd812b5af
Sergey <fserega2014@yandex.ru>
2023-08-16 21:13:00 +0300

    feat: add docker files

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { ConfigModule } from '@nestjs/config';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { FavoriteModule } from './favorite/favorite.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
     ConfigModule.forRoot(),
     UserModule,
     TrackModule,
